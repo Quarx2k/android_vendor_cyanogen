4c55c2b873252eab688e72fd17b15ec080d7dd58
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-06-06 13:06:49 +0200

    use dynamic build date

--- products/cyanogen_jordan.mk
+++ products/cyanogen_jordan.mk
@@ -13,9 +13,6 @@ PRODUCT_DEVICE := jordan
 PRODUCT_MODEL := MB525
 PRODUCT_MANUFACTURER := motorola
 
-PRODUCT_BUILD_UTC_DATE := 1307200000
-PRODUCT_BUILD_UTC_DATE := $(date +%s)
-
 #
 # Set ro.modversion
 #
@@ -32,26 +29,28 @@ else
     endif
 endif
 
-# if somebody could tell me the syntax to use $(date +%s) to set dynamic date in PRODUCT_BUILD_PROP_OVERRIDES... Thanks ;)
+UTC_DATE := $(shell date +%s)
+DATE     := $(shell date +%Y%m%d)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
 BUILD_ID=GRJ22 \
 BUILD_DISPLAY_ID="Gingerbread GRJ22" \
 PRODUCT_NAME=MB525_RTNORD \
 TARGET_DEVICE=umts_jordan \
-BUILD_FINGERPRINT=MOTO/MB525_RTNORD/umts_jordan/jordan:2.3.4/3.4.2-177/1307200000:user/release-keys \
+BUILD_FINGERPRINT=MOTO/MB525_RTNORD/umts_jordan/jordan:2.3.4/3.4.2-177/${UTC_DATE}:user/release-keys \
 PRODUCT_BRAND=MOTO \
-PRIVATE_BUILD_DESC="umts_jordan-user 2.3.4 3.4.2-177 1307200000 release-keys" \
-BUILD_NUMBER=20110604 \
-BUILD_UTC_DATE=1307200000 \
+PRIVATE_BUILD_DESC="umts_jordan-user 2.3.4 3.4.2-177 ${UTC_DATE} release-keys" \
+BUILD_NUMBER=${DATE} \
 BUILD_VERSION_TAGS=release-keys \
 TARGET_BUILD_TYPE=user \
 USER=cyanogen
 
-PRODUCT_COPY_FILES +=  \
+PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/jordan
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/cyanogen/overlay/jordan
 
 # Add the Torch app
- PRODUCT_PACKAGES += Torch
+PRODUCT_PACKAGES += \
+    Torch
