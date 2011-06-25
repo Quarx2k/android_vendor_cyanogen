d0750f7b8a16e95dcd11eb8ad197ee910c9ee23d
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2011-06-25 13:37:57 +0200

    include cyanogen gsm config

--- products/cyanogen_jordan.mk
+++ products/cyanogen_jordan.mk
@@ -24,7 +24,7 @@ ifdef CYANOGEN_NIGHTLY
 else
     ifdef CYANOGEN_RELEASE
         PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-RC2-jordan
+            ro.modversion=CyanogenMod-7.1.0-RC1-jordan
     else
         PRODUCT_PROPERTY_OVERRIDES += \
             ro.modversion=CyanogenMod-7.1.0-RC1-jordan
@@ -37,15 +37,15 @@ DATE     := $(shell date +%Y%m%d)
 PRODUCT_BUILD_PROP_OVERRIDES := \
 BUILD_ID=GRJ22 \
 BUILD_DISPLAY_ID="Gingerbread GRJ22" \
-PRODUCT_NAME=MB525_${PRODUCT_SFX} \
+PRODUCT_NAME=${PRODUCT_MODEL}_${PRODUCT_SFX} \
 TARGET_DEVICE=umts_jordan \
-BUILD_FINGERPRINT=MOTO/MB525_${PRODUCT_SFX}/umts_jordan/jordan:2.3.4/${PRODUCT_SBF}/${UTC_DATE}:user/release-keys \
+BUILD_FINGERPRINT=MOTO/${PRODUCT_MODEL}_${PRODUCT_SFX}/umts_jordan/jordan:2.3.4/${PRODUCT_SBF}/${UTC_DATE}:user/release-keys \
 PRODUCT_BRAND=MOTO \
 PRIVATE_BUILD_DESC="umts_jordan-user 2.3.4 ${PRODUCT_SBF} ${UTC_DATE} release-keys" \
 BUILD_NUMBER=${DATE} \
 BUILD_VERSION_TAGS=release-keys \
 TARGET_BUILD_TYPE=user \
-USER=cyanogen
+USER=CyanogenDefy
 
 PRODUCT_COPY_FILES += \
     vendor/cyanogen/prebuilt/hdpi/media/bootanimation.zip:system/media/bootanimation.zip
@@ -57,6 +57,9 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     Torch
 
+# for custom apns-conf.xml
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
 # TI FM radio
-# $(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
+$(call inherit-product, vendor/cyanogen/products/ti_fm_radio.mk)
 
